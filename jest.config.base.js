df46ca3edf960df7dea4b1c50c25a25d5e2302b8
arvinxx <arvinx@foxmail.com>
2021-02-20 12:52:49 +0800

    :wrench: chore: 调整测试配置项

--- jest.config.base.js
+++ jest.config.base.js
@@ -2,7 +2,8 @@ const path = require('path');
 
 module.exports = {
   preset: 'ts-jest',
-  testEnvironment: 'node',
+
+  // setupFiles: ['./tests/setupTests.ts'],
 
   verbose: true,
   moduleNameMapper: {
