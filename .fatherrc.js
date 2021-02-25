ab7bd080036953100c7162ee6acf602078057b60
arvinxx <arvinx@foxmail.com>
2021-02-26 00:28:14 +0800

    :wrench: chore: 调整打包配置

--- .fatherrc.js
+++ .fatherrc.js
@@ -2,6 +2,6 @@ module.exports = {
   entry: 'src/index.ts',
   cjs: 'babel',
   esm: 'babel',
-  target: 'node',
+  lessInBabelMode: true,
   extraBabelPlugins: [['import', { libraryName: 'antd', style: true }]],
 };
