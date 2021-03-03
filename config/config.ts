b4b1f257dc06d908c6989bcf74a42046940948e3
arvinxx <arvinx@foxmail.com>
2021-03-03 11:50:56 +0800

    :lipstick: style: 优化文档样式

--- config/config.ts
+++ config/config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'umi';
 export default defineConfig({
   title: '空谷的组件库',
   mode: 'site',
+  logo: 'https://gw.alipayobjects.com/zos/antfincdn/mj85r7V5aX/konggu.svg',
   extraBabelPlugins: [
     [
       'import',
@@ -20,6 +21,9 @@ export default defineConfig({
       path: 'https://github.com/arvinxx/components',
     },
   ],
+  theme: {
+    '@c-primary': '#6CB7C7',
+  },
   dynamicImport: {
     loading: '@ant-design/pro-skeleton',
   },
