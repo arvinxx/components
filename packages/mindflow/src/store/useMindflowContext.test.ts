7ad6cd23558d6c83bfb9583948e2862e84eab459
arvinxx <arvinx@foxmail.com>
2021-03-17 16:19:36 +0800

    :white_check_mark: test: 修正测试

--- packages/mindflow/src/store/useMindflowContext.test.ts
+++ packages/mindflow/src/store/useMindflowContext.test.ts
@@ -1,31 +1,32 @@
+import { act, renderHook, cleanup } from '@testing-library/react-hooks';
 import { useMindflowService } from './useMindflowContext';
-import { act, renderHook } from '@testing-library/react-hooks';
 
 describe('useMindflowContext', () => {
+  beforeEach(() => {
+    cleanup();
+  });
   it('初始值', () => {
-    const { result } = renderHook(() => {
-      return useMindflowService();
-    });
+    const { result } = renderHook(() => useMindflowService());
     expect(result.current.collapseList).toEqual([]);
   });
 
   it('添加元素', () => {
-    const { result } = renderHook(() => {
-      return useMindflowService();
-    });
+    const { result } = renderHook(() => useMindflowService());
     act(() => {
       result.current.toggleNodeCollapsed('123');
     });
 
     expect(result.current.collapseList).toEqual(['123']);
-  });
-  it('切换元素', () => {
-    const { result } = renderHook(() => {
-      return useMindflowService();
-    });
+
     act(() => {
       result.current.toggleNodeCollapsed('123');
-      result.current.toggleNodeCollapsed('123');
+    });
+  });
+  it('切换元素', () => {
+    const { result } = renderHook(() => useMindflowService());
+    act(() => {
+      result.current.toggleNodeCollapsed('xxx');
+      result.current.toggleNodeCollapsed('xxx');
     });
 
     expect(result.current.collapseList).toEqual([]);
