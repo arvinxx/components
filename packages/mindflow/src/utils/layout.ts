21ccb0ddec9b0b886d436492ef0336d8a25758cc
arvinxx <arvinx@foxmail.com>
2021-03-17 19:16:48 +0800

    :lipstick: style: 优化节点布局距离

--- packages/mindflow/src/utils/layout.ts
+++ packages/mindflow/src/utils/layout.ts
@@ -13,7 +13,7 @@ export const layout = (dir: 'LR' | 'RL' | 'TB' | 'BT', graph: Graph) => {
   g.setGraph({ rankdir: dir, nodesep: 16, ranksep: 16 });
   g.setDefaultEdgeLabel(() => ({}));
 
-  const width = 300;
+  const width = 330;
   const height = 90;
   nodes.forEach((node) => {
     g.setNode(node.id, { width, height });
