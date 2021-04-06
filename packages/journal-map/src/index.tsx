648ec6cccf7efca6ee868fa2bf03c232e63e4d36
arvinxx <arvinx@foxmail.com>
2021-04-07 00:22:20 +0800

    :sparkles: feat: 添加回调方法

--- packages/journal-map/src/index.tsx
+++ packages/journal-map/src/index.tsx
@@ -3,23 +3,36 @@ import type { FC, CSSProperties } from 'react';
 import cls from 'classnames';
 
 import { Chart, Flow } from './components';
-import { JournalMapStore } from './useJournalMap';
+import { JournalMapStore, useJournalMap } from './useJournalMap';
 import type { JournalMapData } from './type';
 
 export interface JournalMapProps {
   /**
-   * 待渲染数据
+   * 待渲染数据 或网址
    */
-  data: JournalMapData;
+  data: JournalMapData | string;
+  /**
+   * 回调方法
+   * @param data
+   */
+  onChange?: (data: JournalMapData) => void;
+
   style?: CSSProperties;
   className?: string;
 }
 
 export { JournalMapData } from './type';
 
-const JournalMap: FC<JournalMapProps> = ({ data, style, className }) => {
+const JournalMap: FC<JournalMapProps> = ({
+  data,
+  style,
+  className,
+  onChange,
+}) => {
+  const store = useJournalMap(data, onChange);
+
   return (
-    <JournalMapStore.Provider initialState={data}>
+    <JournalMapStore.Provider value={store}>
       <div
         className={cls('avx-journal-map-container', className)}
         style={style}
