2496ea3ed3ecbfd3e79c74c10adfeffda04e9152
arvinxx <arvinx@foxmail.com>
2021-04-18 16:03:11 +0800

    :white_check_mark: test: 添加测试

--- packages/float-label-input/tests/index.spec.tsx
+++ packages/float-label-input/tests/index.spec.tsx
@@ -1,9 +1,39 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { fireEvent, render } from '@testing-library/react';
 
 import FloatLabelInput from '@arvinxu/float-label-input';
 
-test('FloatLabelInput', () => {
-  const { container } = render(<FloatLabelInput label={'test'} />);
-  expect(container).toMatchSnapshot();
+describe('FloatLabelInput', () => {
+  it('正常渲染', () => {
+    const { container } = render(<FloatLabelInput label={'test'} />);
+    expect(container).toMatchSnapshot();
+  });
+
+  it('受控组件', () => {
+    // const { container } = render(<FloatLabelInput label={'test'} />);
+  });
+
+  it('focus 向上移动; blur 返回', () => {
+    const { queryByTestId } = render(<FloatLabelInput label={'test'} />);
+
+    const input = queryByTestId('input');
+    const label = queryByTestId('label');
+
+    expect(label.style.transform).toBe('');
+    fireEvent.focus(input);
+    expect(label.style.transform).toBe('translateY(-28px)');
+    fireEvent.blur(input);
+    expect(label.style.transform).toBe('');
+  });
+
+  it('可输入外部高度', () => {
+    // const { queryByTestId } = render(
+    //   <FloatLabelInput label={'test'} style={{ height: 40 }} />,
+    // );
+    // const input = queryByTestId('input');
+    // fireEvent.focus(input);
+    // const label = queryByTestId('label');
+    // debug(label);
+    // expect(label.style.transform).toBe('translateY(-32px)');
+  });
 });
