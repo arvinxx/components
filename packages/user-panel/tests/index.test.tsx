2bc95678dab934949e47c6b62839080743077c2c
arvinxx <arvinx@foxmail.com>
2021-06-06 17:37:00 +0800

    :white_check_mark: test: 更新测试快照

--- packages/user-panel/tests/index.test.tsx
+++ packages/user-panel/tests/index.test.tsx
@@ -4,9 +4,9 @@ import { render, fireEvent, act } from '@testing-library/react';
 import UserPanel from '@arvinxu/user-panel';
 
 describe('UserPanel', () => {
-  it('默认状态', async () => {
+  it('登录面板', async () => {
     const { container, findByText } = render(
-      <UserPanel
+      <UserPanel.Login
         onClickCaptcha={(mobile) => {
           console.log(mobile);
           return new Promise((resolve) => {
