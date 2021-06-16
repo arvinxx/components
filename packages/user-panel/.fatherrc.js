d0cb034f2a16a9cc58133a7882f84e18ee1afc29
arvinxx <arvinx@foxmail.com>
2021-06-16 22:53:30 +0800

    :wrench: build: 修复构建报错的问题

--- packages/user-panel/.fatherrc.js
+++ packages/user-panel/.fatherrc.js
@@ -2,11 +2,8 @@ const base = require('../../.fatherrc');
 
 module.exports = {
   ...base,
+  lessInBabelMode: false,
   extraBabelPlugins: [
-    [
-      'import',
-      { libraryName: 'antd', libraryDirectory: 'es', style: 'css' },
-      'antd',
-    ],
+    ['import', { libraryName: 'antd', libraryDirectory: 'es', style: true }],
   ],
 };
