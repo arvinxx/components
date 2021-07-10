4fcdd095c2b4e7af6273f7ae137b462407527aa1
arvinxx <arvinx@foxmail.com>
2021-07-11 01:34:20 +0800

    :bug: fix: 修正显示数据的 bug

--- docs/components/biz/examples/Mindflow/Museum.tsx
+++ docs/components/biz/examples/Mindflow/Museum.tsx
@@ -3,7 +3,7 @@ import Mindflow from '@arvinxu/mindflow';
 import * as datasource from './data';
 
 const Museum = () => {
-  return <Mindflow value={datasource.museum} height={600} />;
+  return <Mindflow data={datasource.museum} height={600} />;
 };
 
 export default Museum;
