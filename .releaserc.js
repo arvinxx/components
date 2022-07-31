f76d38064f49fbe41f794b4396a52c34af8ba4b4
arvinxx <arvinx@foxmail.com>
2022-07-31 17:39:32 +0800

    :wrench: chore: 测试发布脚本

--- .releaserc.js
+++ .releaserc.js
@@ -1,4 +1,12 @@
 module.exports = {
   extends: ['semantic-release-config-gitmoji'],
-  branches: ['master', { name: 'feat/*', channel: 'beta', prerelease: 'beta' }],
+  branches: [
+    'master',
+    {
+      name: 'feat/*',
+      channel: 'beta',
+      // 如果存在多个feat/ prerelease
+      prerelease: true,
+    },
+  ],
 };
